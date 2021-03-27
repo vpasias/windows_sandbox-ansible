28a88392fa60624213f863b008821f59783a8fa0
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-27 12:23:36 +0200

    Update Setup-Ansible.sh

--- Scripts/Setup-Ansible.sh
+++ Scripts/Setup-Ansible.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get install ansible python-pip sshpass -y
+DEBIAN_FRONTEND=noninteractive apt update
+DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-simplejson python3-jinja2 python3-dev python3-venv python3-pip libffi-dev gcc libssl-dev curl git vim sshpass
+pip3 install -U pip
+
+pip3 install --upgrade pip
+pip install -U 'ansible<2.10'
+
 if ! [ -L /etc/ansible ]; then
   rm -rf /etc/ansible
   ln -fs /vagrant/ansible /etc/ansible
